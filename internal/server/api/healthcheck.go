2f318cdb1de9d32fad7d19211a2fff1b2c1fdeb9
Roman Kovach <idcfyoy@gmail.com>
2025-11-08 07:17:40 +0300

    feat: made healthcheck endpoint better

--- internal/server/api/healthcheck.go
+++ internal/server/api/healthcheck.go
@@ -1,14 +1,29 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type HealthCheckRequest struct {
+	Echo string `form:"echo"`
 }
 
 type HealthCheckResponse struct {
 	Status string `json:"status"`
+	Echo   string `json:"echo,omitempty"`
+}
+
+type ErrorResponse struct {
+	ErrorMessage string `json:"error_message"`
 }
 
 func (api *API) HealthCheck(c *gin.Context) {
-	c.JSON(200, HealthCheckResponse{Status: "ok"})
+	var request HealthCheckRequest
+	if err := c.ShouldBindQuery(&request); err != nil {
+		c.JSON(http.StatusBadRequest, ErrorResponse{ErrorMessage: "Invalid input"})
+		return
+	}
+	c.JSON(http.StatusOK, HealthCheckResponse{Status: "ok", Echo: request.Echo})
 }
